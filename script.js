63496db2cd18b6e126b710645cbe019890f77d12
keyshaathallias <keyshaathallias@gmail.com>
2024-09-05 15:26:07 +0700

    Exercise 4

--- script.js
+++ script.js
@@ -47,3 +47,11 @@ sBlue.addEventListener('input', function() {
     const b = sBlue.value;
     document.body.style.backgroundColor = 'rgb('+ r +', '+ g +', '+ b +')';
 })
+
+// Exercise 4
+document.body.addEventListener('mousemove', function(event) {
+    const xPos = Math.round((event.clientX / window.innerWidth) * 255);
+    const yPos = Math.round((event.clientY / window.innerHeight) * 255);
+
+    document.body.style.backgroundColor = 'rgb('+ xPos +', '+ yPos +', 100)';
+})
