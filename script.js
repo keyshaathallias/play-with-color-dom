6febc3b5bd1be8e6377849a4a1b903d7cfe5c6ce
keyshaathallias <keyshaathallias@gmail.com>
2024-09-05 14:49:05 +0700

    exercise 1

---
+++ script.js
@@ -0,0 +1,6 @@
+// Exercise 1
+const btnChangeColor = document.getElementById('btnChangeColor');
+
+btnChangeColor.addEventListener('click', function() {
+    document.body.classList.toggle('light-blue');
+})
